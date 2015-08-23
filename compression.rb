0d554a8ce20c7bcaee49c35e8c847ff2d4ff4464
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-23 08:51:21 -0700

    CLI implemented

--- compression.rb
+++ compression.rb
@@ -1,21 +1,25 @@
 class Compressor
   attr_reader :occurences
 
-  def compress(filepath)
+  def filepath
+    ARGV[0]
+  end
+
+  def compress
     Dir.chdir(File.dirname(__FILE__))
-
     file_array = []
-
     File.open(filepath, "r") do |f|
       f.each_line { |line| file_array << line }
     end
-
     string = file_array.join(" ").split(" ")
-
     @occurences = {}
-    string.each_with_index do |word, i|
+    new_file = string.each_with_index do |word, i|
       @occurences[word] ? @occurences[word] << i : @occurences[word] = [i]
     end
+    puts "hey"
+    out_file = File.new("compressed_#{filepath}", "w")
+    out_file.puts(new_file)
+    out_file.close
   end
 
   def uncompress
@@ -24,15 +28,7 @@ class Compressor
       occurences.each { |o| arr[o] ? arr[o] << word : arr[o] = word }
     end
     arr.join(" ")
-    #hash = hash.to_s.delete(" ")                                       # => "{\"hello\"=>[0,1,2,3,4,5,6,7,8,9,10,11,12]}"
-    #File.open("compressed.txt", "w") {|f| f.write(hash)}               # => 41
   end
 end
-c = Compressor.new
-c.compress("test.txt")
-c.occurences
-c.uncompress
 
-Dir.chdir(File.dirname(__FILE__))
-File.size?("compressed.txt")
-File.size?("test.txt")
+Compressor.new.compress
