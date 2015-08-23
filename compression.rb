0dfb715f2e2ef6a6d571c16928a8df74ef933c8b
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-23 08:52:01 -0700

    delete RDD puts

--- compression.rb
+++ compression.rb
@@ -16,7 +16,6 @@ class Compressor
     new_file = string.each_with_index do |word, i|
       @occurences[word] ? @occurences[word] << i : @occurences[word] = [i]
     end
-    puts "hey"
     out_file = File.new("compressed_#{filepath}", "w")
     out_file.puts(new_file)
     out_file.close
